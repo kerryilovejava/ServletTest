e3ab5aa3de039501a40b49f5d7bf3d5952980486
zhujinfan <1023846358@qq,com>
2018-07-31 10:14:37 +0800

    jar测试

---
+++ src/com/projectJar/demo1.java
@@ -0,0 +1,16 @@
+package com.projectJar;
+
+import com.dahan.demo;
+
+/**
+ * @Description:
+ * @Author: 5385
+ * @Date 2018/7/31
+ * @Time 10:13
+ */
+public class demo1 {
+    public static void main(String[] args) {
+        String show = demo.show();
+        System.out.println(show);
+    }
+}
